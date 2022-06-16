fc6f80780495cf1c2cac00fbedc01ec4936ab64e
take-game <79352953+take-game@users.noreply.github.com>
2022-06-16 09:30:11 +0900

    Update bmesse.js

--- js/bmesse.js
+++ js/bmesse.js
@@ -20,7 +20,7 @@ $(document).ready(function() {//htmlの描画が終わったら、処理を実�
         p('<div id="bms_chat_header">');
             p('<div id="bms_chat_user_status">');
                 p('<div id="bms_status_icon">●</div>')
-                p('<div id="bms_chat_user_name">ユーザー</div>');
+                p('<div id="bms_chat_user_name">user</div>');
             p('</div>');
         p('</div>');
               //タイムライン部分③
@@ -30,7 +30,7 @@ $(document).ready(function() {//htmlの描画が終わったら、処理を実�
             p('<div class="bms_message bms_left">');
                 p('<div class="bms_message_box">');
                     p('<div class="bms_message_content">');
-                        p('<div class="bms_message_text">ほうほうこりゃー便利じゃないか</div>');
+                        p('<div class="bms_message_text">こんにちは。お元気ですか。</div>');
                     p('</div>');
                 p('</div>');
             p('</div>');
@@ -40,7 +40,7 @@ $(document).ready(function() {//htmlの描画が終わったら、処理を実�
             p('<div class="bms_message bms_right">');
                 p('<div class="bms_message_box">');
                     p('<div class="bms_message_content">');
-                        p('<div class="bms_message_text">うん、まあまあいけとるな</div>');
+                        p('<div class="bms_message_text">ええ！元気ですわ！！あなたは！？</div>');
                     p ('</div>');
                 p('</div>');
             p('</div>');
